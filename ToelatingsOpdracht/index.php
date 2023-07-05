67630081bfab31d18751ca7b885e63adc162a4fc
JoeyBerglund <114988875+JoeyBerglund@users.noreply.github.com>
2023-07-05 17:18:34 +0200

    Add files via upload

---
+++ ToelatingsOpdracht/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css" />
+    <script src="script.js" defer></script>
+  </head>
+  <header class="header">
+    <div class="topLayer">
+    <?php require_once("navbar.php"); ?>
+    <div class="topLayer-text">
+        <img id="LogoDijkEnWaard" src="img/DijkenWaard.png" alt="LogoDijkEnWaard"/>
+        <button id="BekijkVacatures">bekijk beschikbare vacatures</button>
+      </div>
+    </div>
+  </header>
+  <body>
+
+  </body>
+</html>
---
+++ ToelatingsOpdracht/navbar.php
@@ -0,0 +1,28 @@
+
+<nav class="navbar">
+      <div class="loginOrRegister">
+        <a href="#" class="nav-logo">Login</a>
+        <p class="nav-logo">|</p>
+        <a href="#" class="nav-logo">Registreren</a>
+      </div>
+
+      <ul class="nav-menu">
+        <li class="nav-item">
+          <a href="index.php" class="nav-link">Home</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">About</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Contact</a>
+        </li>
+      </ul>
+      <div class="hamburger">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+      </div>
+    </nav>
